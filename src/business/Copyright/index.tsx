cf1a059f32225b12a8285eca74299d5d0704c641
pengdaokuan <1063137960@qq.com>
2019-11-29 18:00:50 +0800

    [feature][what]修改业务组件，接受redux主题色调

--- src/business/Copyright/index.tsx
+++ src/business/Copyright/index.tsx
@@ -4,17 +4,36 @@
  * @CreateDate:  2019-11-28
  * @LastModify:  2019-11-28
  */
-import React from 'react'
-import styles from './index.module.css'
-import { copyright } from '../../helper/constant'
+import React from "react";
+import styles from "./index.module.css";
+import { copyright } from "../../helper/constant";
 
-export default function Copyright() {
-  return (
-    <div className={styles.footer}>
-      <p className={styles.copyright}>
-        Copyright © {copyright.FROMDATE}-{copyright.CURRENTDATE}
-        All Rights Reserved. Copyright By pengdaokuan
-      </p>
-    </div>
-  )
+/**
+ * @class Copyright
+ * @extends {React.Component}
+ * @property {string} bgColor - 背景色
+ * @property {string} textColor - 文案颜色
+ */
+interface Props {
+  bgColor: string;
+  textColor: string;
+}
+
+export default class Copyright extends React.PureComponent<Props> {
+  render() {
+    return (
+      <div
+        className={styles.footer}
+        style={{
+          backgroundColor: this.props.bgColor,
+          color: this.props.textColor
+        }}
+      >
+        <p className={styles.copyright}>
+          Copyright © {copyright.FROMDATE}-{copyright.CURRENTDATE}
+          All Rights Reserved. Copyright By pengdaokuan
+        </p>
+      </div>
+    );
+  }
 }
