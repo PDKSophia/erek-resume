b455fc7d0b78bfe2430be8bf1d61233b6107fe05
pengdaokuan <1063137960@qq.com>
2019-11-29 18:00:00 +0800

    [feature][what]添加默认配置的一些interface

---
+++ src/helper/interface.ts
@@ -0,0 +1,16 @@
+/**
+ * @Desc: 默认配置的 constant interface
+ * @Author: pengdaokuan
+ * @CreateDate:  2019-11-29
+ * @LastModify:  2019-11-29
+ */
+export interface ThemeStateFace {
+  bgColor: string;
+  textColor: string;
+  hoverColor: string;
+  uniqueKey: string;
+}
+
+export interface ThemeListFace {
+  [key: string]: any;
+}
