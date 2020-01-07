01628a9f10e763d629612daf2773aec130c7ad05
pengdaokuan <1063137960@qq.com>
2020-01-07 10:49:17 +0800

    [feature][what]模板组件引入Menu组件

--- src/client/Template/index.tsx
+++ src/client/Template/index.tsx
@@ -2,29 +2,30 @@
  * @Desc: 模板组件
  * @Author: pengdaokuan
  * @CreateDate:  2019-12-01
- * @LastModify:  2019-12-01
+ * @LastModify:  2020-01-07
  */
-import React from 'react'
-import styles from './index.module.css'
-import classnames from 'classnames/bind'
-import { useSelector } from 'react-redux'
-import { AppStoreType } from '../../store/reducers'
-import { product } from "../../lib/constant";
+import React from "react";
+import styles from "./index.module.css";
+import classnames from "classnames/bind";
+import { useSelector } from "react-redux";
+import { AppStoreType } from "../../store/reducers";
+import { TEMPLATE_MENU, TEMPLATE } from "../../lib/constant";
 import Image from "../../common/components/Image";
 import Introduce from "../../business/Introduce";
+import Menu from "../../business/Menu";
 import Copyright from "../../business/Copyright";
-let cx = classnames.bind(styles)
+let cx = classnames.bind(styles);
 
-export default function Template() {
+export default function Template(props: any) {
   const currentTheme = useSelector(
     (state: AppStoreType) => state.themeStore.theme
-  )
+  );
 
   return (
     <div
       className={cx({
         container: true
-     })}
+      })}
       style={{
         backgroundColor: currentTheme.bgColor,
         color: currentTheme.textColor
@@ -36,7 +37,7 @@ export default function Template() {
           flex: true
         })}
       >
-        <Image cover={product.LOGO} />
+        <Image cover={TEMPLATE.LOGO} />
       </div>
       <div
         className={cx({
@@ -44,8 +45,8 @@ export default function Template() {
         })}
       >
         <Introduce
-          title={product.TITLE}
-          summary={product.SUMMARY}
+          title={TEMPLATE.TITLE}
+          summary={TEMPLATE.SUMMARY}
           style={{
             textAlign: "center",
             backgroundColor: currentTheme.bgColor,
@@ -53,6 +54,13 @@ export default function Template() {
           }}
         />
       </div>
+      <div
+        className={cx({
+          menu: true
+        })}
+      >
+        <Menu list={TEMPLATE_MENU} columns={3} history={props.history} />
+      </div>
     </div>
-  )
+  );
 }
