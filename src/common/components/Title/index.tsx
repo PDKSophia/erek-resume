80aaddade591ba3e3550f9b8be3f1bb1a1e90365
pengdaokuan <1063137960@qq.com>
2020-01-09 17:15:14 +0800

    [feature][what]添加Title组件

---
+++ src/common/components/Title/index.tsx
@@ -0,0 +1,71 @@
+/**
+ * @Desc: 模块标题组件
+ * @Author: pengdaokuan
+ * @CreateTime: 2020-01-09
+ * @LastModify: 2020-01-09
+ */
+import React from "react";
+import styles from "./index.module.css";
+import classnames from "classnames/bind";
+import { connect } from "react-redux";
+import { AppStoreType } from "../../../store/reducers";
+import { TitleStateFace } from "../../../config/interface";
+import { titleProps } from "../../constants/defaultProps";
+const cx = classnames.bind(styles);
+
+/**
+ * @class Title
+ * @extends {React.Component}
+ * @property {string} title - 标题
+ * @property {string} size - 文字大小 little/small/middle/big/super
+ * @property {object} style - 样式
+ * @property {string} summary - 简介
+ * @property {boolean} underline - 文字底下横线
+ */
+
+class Title extends React.PureComponent<TitleStateFace> {
+  public static namespace: string = "标题组件";
+  public static defaultProps: TitleStateFace = {
+    title: titleProps.TITLE,
+    size: titleProps.SIZE,
+    underline: titleProps.UNDERLINE
+  };
+  render() {
+    const { theme = {} } = this.props;
+    const { title, summary, underline, size, style } = this.props;
+    return (
+      <div className={cx("container")}>
+        <div
+          className={cx("title", {
+            [`title-${size}`]: true
+          })}
+          style={style}
+        >
+          {title}
+        </div>
+        {summary && (
+          <div
+            className={cx("content", {
+              [`content-${size}`]: true
+            })}
+          >
+            {summary}
+          </div>
+        )}
+        {underline && (
+          <div
+            className={cx("underline")}
+            style={{ backgroundColor: theme.bgColor }}
+          />
+        )}
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state: AppStoreType) => {
+  return {
+    theme: state.themeStore.theme
+  };
+};
+export default connect(mapStateToProps)(Title);
