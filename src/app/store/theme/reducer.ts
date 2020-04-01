c48363d3f2644d84a03f8ce4f1a6fe4b02e191a7
pengdaokuan <1063137960@qq.com>
2020-04-01 13:16:00 +0800

    [feature][what]修改redr,由merge改为replace

--- src/app/store/theme/reducer.ts
+++ src/app/store/theme/reducer.ts
@@ -1,8 +1,8 @@
 /**
- * @Desc: 主题 theme reducer
- * @Author: pengdaokuan
- * @CreateTime: 2020-03-28
- * @LastModify: 2020-03-28
+ * @desc 主题 theme reducer
+ * @author pengdaokuan
+ * @createTime 2020-03-28
+ * @lastModify 2020-04-01
  */
 import Immutable from "seamless-immutable";
 import * as actionTypes from "../actionsType";
@@ -24,7 +24,7 @@ function themeStore(
   const payload = action.payload || {};
   switch (action.type) {
     case actionTypes.STORE_PROPS:
-      return state.merge(state, payload);
+      return Immutable.replace(state, payload);
     default:
       return state;
   }
